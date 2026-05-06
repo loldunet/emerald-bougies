d0b0030086127a4309799862c8579f0bd1dcd55e
loldunet <mrl.matthieu@gmail.com>
2026-05-06 21:45:52 +0200

    Keep-alive ping Render toutes les 10 min

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './style.css'
 import './app.css'
 import App from './App'
+import { API_URL } from './config/api'
+
+// Ping le backend toutes les 10 min pour éviter le cold start Render
+const keepAlive = () => fetch(`${API_URL.replace('/api', '')}/health`).catch(() => {})
+keepAlive()
+setInterval(keepAlive, 10 * 60 * 1000)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
